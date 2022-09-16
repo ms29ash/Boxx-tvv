58b2a530cb4472c25c65e16651af891a6440324e
ms29ash <ms29ash@gmail.com>
2022-09-16 11:36:09 +0530

    add hover effect in categories

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -11,7 +11,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   display: flex;
   width: 100%;
-  flex-wrap:wrap;
+  flex-wrap: wrap;
 `;
 
 const Wrap = styled(Link)`
@@ -19,57 +19,80 @@ const Wrap = styled(Link)`
   margin: 0.5%;
   display: grid;
   place-items: center;
-  border-radius: 10px;
-  aspect-ratio: 1/1;
-  border: 2px solid #180f11;
+  border-radius: 50px;
+  aspect-ratio: 1/0.25;
   overflow: hidden;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   position: relative;
   cursor: pointer;
-  text-decoration:none;
-  color:#fff;
-  background: hsla(339, 100%, 55%, 1);
-  @media screen and (max-width: 768px) and (min-width: 0){
-      width:31%;
-      margin:2% 0.50%;
-
-    }
+  text-decoration: none;
+  color: #fff;
+  border: 2px solid white;
+  @media screen and (max-width: 768px) and (min-width: 0) {
+    border: 2px solid ${(p) => p.theme.color.background};
+  }
+  @media screen and (max-width: 768px) and (min-width: 0) {
+    width: 31%;
+    margin: 2% 0.5%;
+  }
   &:hover {
     border: 2px solid ${(p) => p.theme.color.main};
-   
   }
-  
+
   p {
     display: grid;
     place-items: center;
     z-index: 3;
     font-weight: bold;
-    font-size: 1.25rem;
+    font-size: 1rem;
     height: 100%;
     width: 100%;
-    
-    @media screen and (max-width: 768px) and (min-width: 0){
-      border-radius: 5px;
-
+    @media screen and (max-width: 720px) and (min-width: 0) {
+      font-size: 0.8rem;
     }
   }
 `;
 
 const Action = styled(Wrap)`
-  background: ${(p) => `linear-gradient(to right, #f953c6, #b91d73);`};
+  &:hover {
+    background: linear-gradient(to right, #f953c6, #b91d73);
+  }
+  @media screen and (max-width: 768px) and (min-width: 0) {
+    background: linear-gradient(to right, #f953c6, #b91d73);
+  }
 `;
 
 const Adventure = styled(Wrap)`
-  background: ${(p) => ` linear-gradient(to right, #8e2de2, #4a00e0);`};
+  &:hover {
+    background: linear-gradient(to right, #8e2de2, #4a00e0);
+  }
+  @media screen and (max-width: 768px) and (min-width: 0) {
+    background: linear-gradient(to right, #8e2de2, #4a00e0);
+  }
 `;
 const SciFi = styled(Wrap)`
-  background: ${(p) => `linear-gradient(to right, #f12711, #f5af19)`};
+  &:hover {
+    background: linear-gradient(to right, #f12711, #f5af19);
+  }
+  @media screen and (max-width: 768px) and (min-width: 0) {
+    background: linear-gradient(to right, #f12711, #f5af19);
+  }
 `;
 const Thriller = styled(Wrap)`
-  background: ${(p) => `linear-gradient(to right, #ed213a, #93291e)`};
+  &:hover {
+    background: linear-gradient(to right, #ed213a, #93291e);
+  }
+  @media screen and (max-width: 768px) and (min-width: 0) {
+    background: linear-gradient(to right, #ed213a, #93291e);
+  }
 `;
 const Crime = styled(Wrap)`
-  background: ${(p) => ` linear-gradient(to right, #00c6ff, #0072ff);`};
+  &:hover {
+    background: linear-gradient(to right, #00c6ff, #0072ff);
+  }
+  @media screen and (max-width: 768px) and (min-width: 0) {
+    background: linear-gradient(to right, #00c6ff, #0072ff);
+  }
 `;
 function Categories() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { Provider } from 'react-redux'
-import { store } from './app/store'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 const theme = {
   color: {
     main: "#f20544",
-    mainDark: "#c00436"
-
+    mainDark: "#c00436",
+    background: "#180f11",
   },
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Provider store={store}>
           <QueryClientProvider client={queryClient}>
-
             <App />
           </QueryClientProvider>
         </Provider>
