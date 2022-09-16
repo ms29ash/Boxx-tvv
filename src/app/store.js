57cfbdc0d3538638926e67efdb813b97f4aebe6b
ms29ash <ms29ash@gmail.com>
2022-09-16 19:55:17 +0530

    add favourite slice

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import menuReducer from '../features/menu/menuSlice'
 import userReducer from '../features/user/userSlice'
 import watchlistReducer from '../features/watchlist/watchlistSlice'
+import favouriteReducer from '../features/watchlist/favouriteSlice'
 export const store = configureStore({
     reducer: {
         menu: menuReducer,
         user: userReducer,
-        watchlist: watchlistReducer
+        watchlist: watchlistReducer,
+        favourite: favouriteReducer
     },
 })
