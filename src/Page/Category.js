003ec6eab5d434de4fe5463ac8c6ac3abeaa9e9d
ms29ash <ms29ash@gmail.com>
2022-09-25 02:12:05 +0530

    Add
    Top Background in Category

--- src/Page/Category.js
+++ src/Page/Category.js
@@ -8,30 +8,25 @@ const Container = styled.div``;
 const TopBackground = styled.div`
   width: 100%;
   aspect-ratio: 8/2;
-  /* background: url(${(p) =>
-    `/images/${p.bg}-background.jpg`}) no-repeat center
-    center/cover; */
   position: relative;
   margin-bottom: 5rem;
-  z-index: -1;
+  display:grid;
+  place-items:center;
   @media screen and (min-width: 0px) and (max-width: 1024px) {
     aspect-ratio: 5/1;
   }
-  &:before {
-    position: absolute;
-    content: "";
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    background: linear-gradient(
-      to right,
-      rgba(0, 0, 0, 0.9) 0%,
-      rgba(0, 0, 0, 0) 100%
-    );
+
+    ${p => p.bg === 'action' ? 'background: linear-gradient(to right, #f953c6, #b91d73);' :
+    p.bg === 'adventure' ? 'background: linear-gradient(to right, #8e2de2, #4a00e0);' :
+      p.bg === 'sci-fi' ? 'background: linear-gradient(to right, #f12711, #f5af19);' :
+        p.bg === 'thriller' ? 'background: linear-gradient(to right, #ed213a, #93291e);' :
+          p.bg === 'crime' ? 'background: linear-gradient(to right, #00c6ff, #0072ff);' : ''
   }
+  
   h3 {
     color: #fff;
+    text-transform:capitalize;
+    font-size: 3rem;
   }
 
   img {
@@ -49,6 +44,7 @@ const TopBackground = styled.div`
     object-position: 20%;
     text-align: center;
   }
+  
 `;
 
 function Category() {
@@ -57,8 +53,7 @@ function Category() {
   return (
     <Container>
       <TopBackground bg={path[2]}>
-        {/* <img src={`/images/${path[2]}-logo.png`} alt="" /> */}
-        <img src="https://wallpaper.dog/large/17112733.jpg" alt="" />
+
         <h3>{path[2]}</h3>
       </TopBackground>
       <ImageSlider title="movies" />
@@ -68,3 +63,4 @@ function Category() {
 }
 
 export default Category;
+
